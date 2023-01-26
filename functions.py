adad43995aacbb1deed886baf06debc3ab97014c
0xphoton <42502920+maxtalwar@users.noreply.github.com>
2023-01-25 17:36:07 -0800

    added port info
    added info about appropriate ports for windows devices

--- functions.py
+++ functions.py
@@ -16,5 +16,7 @@ def start():
 def stop_bot():
     bot.drive_stop()
 
+# port = "/dev/cu.usbserial-DN028709" <- on Most Mac Devices
+# port = "COM1", "COM2", "COM3", or "COM4" <- on most windows devices. You can find the connected devices in device manager under ports, one of the device names should be the one you choose here (eg COM3)
 port = "/dev/cu.usbserial-DN028709"
 bot = Create2(port)
