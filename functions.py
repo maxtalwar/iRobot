b17cb6d27b0b9f5e82d2ad3445296f79d2acce87
0xphoton <42502920+maxtalwar@users.noreply.github.com>
2023-01-25 16:38:01 -0800

    finalized code

---
+++ functions.py
@@ -0,0 +1,20 @@
+import time
+from pycreate2 import Create2
+
+def drive(left, right):
+    bot.drive_direct(left, right)
+    time.sleep(2)
+
+def turn(amount):
+    bot.drive_direct(100*amount, -100*amount)
+    time.sleep(2)
+
+def start():
+    bot.start()
+    bot.safe()
+
+def stop_bot():
+    bot.drive_stop()
+
+port = "/dev/cu.usbserial-DN028709"
+bot = Create2(port)
---
+++ iRobot.py
@@ -0,0 +1,13 @@
+from functions import start, drive, turn, stop_bot
+
+# start the robot
+start()
+
+# drive the robot
+drive(left=100, right=100)
+
+# turn the robot
+turn(amount=1)
+
+# stop the robot
+stop_bot()
